012ff5b7fcbecef775758c1054a154ace7f36024
Martin Tasker <martin.tasker@databatix.com>
2016-10-27 14:34:46 +0100

    setup: query, shows initially whether there are usable roles in ID pool

---
+++ setup/query.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var AWS = require('aws-sdk');
+
+var config = require('./lib/config');
+var settings = require('./lib/settings');
+
+AWS.config.region = config.MY_REGION;
+
+var bucket = new AWS.S3({
+  params: {
+    Bucket: config.BUCKET_NAME,
+    region: config.BUCKET_REGION,
+  }
+});
+
+var cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
+var cognitoIdentity = new AWS.CognitoIdentity();
+
+Promise.resolve()
+  .then(function() {
+    return queryIdentityPoolRoles(settings.get('identityPoolId'));
+  })
+  .catch(function(reason) {
+    console.log("problem: %j", reason);
+  });
+
+function queryIdentityPoolRoles(identityPoolId) {
+  return new Promise(function(resolve, reject) {
+    cognitoIdentity.getIdentityPoolRoles({
+      IdentityPoolId: identityPoolId,
+    }, function(err, data) {
+      if (err) {
+        return reject(err);
+      }
+      console.log("queryIdentityPoolRoles -> %j", data);
+      return resolve(data);
+    });
+  });
+}
