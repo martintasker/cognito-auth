fb3fc5d633d018777b64c61dda4e7919c15210cd
Martin Tasker <martin.tasker@databatix.com>
2016-10-27 17:30:22 +0100

    app: adopt values from latest pool/bucket setup

--- demo/app/scripts/app.js
+++ demo/app/scripts/app.js
@@ -7,10 +7,10 @@ angular.module('demoApp', [
 
 .constant('CognitoAuthConfig', {
   AWS_REGION: 'eu-west-1',
-  AWS_USER_POOL_ID: 'eu-west-1_eetM2eus0', // arn:aws:cognito-idp:eu-west-1:564628766628:userpool/<id> for ARN
+  AWS_USER_POOL_ID: 'eu-west-1_QwNyuBilG',
   // note: FBEmmAdminAuth pre-signup lambda prevents arbitrary registration into above user pool
-  AWS_APP_ID: '26qrure8tus888itr3ou66eci0',
-  AWS_ID_POOL_ID: 'eu-west-1:6ac29173-d3b8-4bf5-8df9-e4ba50bf7184',
+  AWS_APP_ID: '66m8segbilu6jmbvc8fptl1lmd',
+  AWS_ID_POOL_ID: 'eu-west-1:2f344f65-d141-4c9f-9e1c-ea3ce86fcbd7',
 })
 
 ;
