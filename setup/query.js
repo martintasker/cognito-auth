4529b40a8bb646d65c777295848b18dc9dcc9e0b
Martin Tasker <martin.tasker@databatix.com>
2016-11-14 17:39:53 +0000

    setup: query for users

--- setup/query.js
+++ setup/query.js
@@ -8,23 +8,40 @@ var settings = require('./lib/settings');
 AWS.config.region = config.REGION;
 
 var cognitoIdentity = new AWS.CognitoIdentity();
+var cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
 
 Promise.resolve()
-  .then(queryIdentityPoolRoles)
+.then(queryIdentityPoolRoles)
+.then(queryUsers)
   .catch(function(reason) {
     console.log("problem: %j", reason);
   });
 
 function queryIdentityPoolRoles() {
-  var identityPoolId = settings.get('identityPoolId');
   return new Promise(function(resolve, reject) {
     cognitoIdentity.getIdentityPoolRoles({
-      IdentityPoolId: identityPoolId,
+      IdentityPoolId: settings.get('identityPoolId'),
     }, function(err, data) {
       if (err) {
         return reject(err);
       }
       console.log("queryIdentityPoolRoles -> %j", data);
+      return resolve(data);
+    });
+  });
+}
+
+function queryUsers() {
+  return new Promise(function(resolve, reject) {
+    cognitoIdentityServiceProvider.listUsers({
+      UserPoolId: settings.get('userPoolId'),
+      AttributesToGet: ['email'],
+      PaginationToken: null // todo: scale up to support multiple queries using pagination tokens
+    }, function(err, data) {
+      if (err) {
+        return reject(err);
+      }
+      console.log("listUsers -> %j", data);
       return resolve(data);
     });
   });
